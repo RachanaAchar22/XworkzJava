c7008b3f87853fd65d7ee10109259df3d1a18900
Rachanaachar22 <rachanaachar.xworkz@gmail.com>
2023-07-20 11:20:06 +0530

    first commit

---
+++ Badminton.java
@@ -0,0 +1,14 @@
+class Badminton{
+	int noOfPlayers;
+	String playerName;
+	String stadiumName;
+	String brand;
+	int people;
+	
+	Badminton()
+	{
+		System.out.println("Invoking main in Badminton");
+	}
+	
+	
+}
